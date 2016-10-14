a24b0bcca0a8b1c3f09cb13c17c4aeacdf1f9c68
Sameer Rahmani <lxsameer@gnu.org>
2016-10-14 23:23:21 +0330

    foriegn libs copied from the garm project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(ns )
+
+                    :foreign-libs [{:file "src/js/wow/wow.js"
+                                    :provides ["js.vendor.wow"]}
+                                   {:file "src/js/waves/waves.js"
+                                    :provides ["js.vendor.waves"]}
+                                   {:file "src/js/jq/slimscroll.js"
+                                    :provides ["js.vendor.slimscroll"]}]
