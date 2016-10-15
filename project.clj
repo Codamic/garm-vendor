3769d6292ec1207afb161bb8cd1392843d51289f
Sameer Rahmani <lxsameer@gnu.org>
2016-10-15 21:11:07 +0330

    first PoC is done

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
-(ns )
-
-                    :foreign-libs [{:file "src/js/wow/wow.js"
-                                    :provides ["js.vendor.wow"]}
-                                   {:file "src/js/waves/waves.js"
-                                    :provides ["js.vendor.waves"]}
-                                   {:file "src/js/jq/slimscroll.js"
-                                    :provides ["js.vendor.slimscroll"]}]
+(defproject codamic/garm-vendor "0.1.0"
+  :description "External dependencies of Garm"
+  :url "http://github.com/Codamic/garm-vendor"
+  :license {:name "GNU Public License v2"
+            :url "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"})
---
+++ src/deps.cljs
@@ -0,0 +1,8 @@
+{
+ :foreign-libs [{:file "wow/wow.js"
+                 :provides ["garm.vendor.js.wow"]}
+                {:file "waves/waves.js"
+                 :provides ["garm.vendor.waves"]}
+                {:file "jquery/slimscroll.js"
+                 :provides ["garm.vendor.js.slimscroll"]}]
+ }
