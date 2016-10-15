71205f38a584452719b07c71f839fcbd42949104
Sameer Rahmani <lxsameer@gnu.org>
2016-10-15 21:24:33 +0330

    deps.cljs minor bug fixed

--- src/deps.cljs
+++ src/deps.cljs
@@ -2,7 +2,7 @@
  :foreign-libs [{:file "wow/wow.js"
                  :provides ["garm.vendor.js.wow"]}
                 {:file "waves/waves.js"
-                 :provides ["garm.vendor.waves"]}
+                 :provides ["garm.vendor.js.waves"]}
                 {:file "jquery/slimscroll.js"
                  :provides ["garm.vendor.js.slimscroll"]}]
  }
